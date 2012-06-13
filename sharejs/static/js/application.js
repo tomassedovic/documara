bebb34d0c1569103d32840f772fba75d94dad45f
Tomas Sedovic <tomas@sedovic.cz>
2012-06-13 23:11:28 +0200

    Hook the auth stuff to the UI
    When the user's not logged in, we display the form. After they do log
    in, they get to see their document.

--- sharejs/static/js/application.js
+++ sharejs/static/js/application.js
@@ -1,4 +1,21 @@
-$(document).ready(function() {
+$('#login-page form').live('submit', function() {
+  var $form = $(this);
+  var params = $form.serialize();
+  $.ajax({
+    url: $form.attr('action'),
+    type: 'POST',
+    data: params,
+    success: function() {
+      openDocument();
+    },
+    error: function() {
+      $form.find('.alert').show();
+    }
+  });
+  return false;
+});
+
+function openDocument() {
   sharejs.open('thingy', 'text', function(err, doc) {
     if(err) {
       console.log("Error connecting ShareJS:", err);
@@ -13,6 +30,13 @@ $(document).ready(function() {
       // ShareJS doesn't support jQuery objects, we must use the native DOM
       // representation for $editor
       doc.attach_textarea($editor[0]);
+      $('#login-page').hide();
+      $('#document-page').show();
     }
-  })
+  });
+}
+
+
+$(document).ready(function() {
+  openDocument();
 })
